013a89156b88a34628b207ac435a9253f245842a
edumoreira1506 <contato@eduardoem.com.br>
2019-08-15 17:28:56 -0300

    Fazendo o cadastro via ajax no arquivo mulheres.json

--- app/controllers/womans.js
+++ app/controllers/womans.js
@@ -1,12 +1,54 @@
 module.exports = app => {
 
     const index = (req, res) => {
-    	res.render('womans/index')
+        res.render('womans/index')
     }
 
     const register = (req, res) => {
         res.render('womans/new')
     }
 
-    return { index, register }
+    const create = (req, res) => {
+
+        if (!req.body.name || !req.body.email) {
+            res.send({
+                status: false,
+                message: 'Nome e e-mail são obrigatórios!'
+            })
+        } else {
+            const fs = require('fs')
+            const jsonPath = '././public/mulheres.json'
+
+            fs.exists(jsonPath, function (exists) {
+                fs.readFile(jsonPath, function readFileCallback(err, data) {
+                    let array = JSON.parse(data);
+                    array.mulheres.push({
+                        "name": req.body.name,
+                        "interests": req.body.qualitys,
+                        "location": req.body.location,
+                        "photo": req.body.name,
+                        "email": req.body.email,
+                        "linkedin": req.body.linkeDin,
+                        "github": req.body.gitHub,
+                        "twitter": req.body.twitter,
+                        "fb": req.body.faceBook,
+                        "behance": req.body.behance,
+                        "site": req.body.site
+                    })
+                    let json = JSON.stringify(array);
+                    fs.writeFile(jsonPath, json);
+                });
+            });
+
+            res.send({
+                status: true
+            })
+        }
+    }
+
+    return {
+        index,
+        register,
+        create
+    }
 }
--- config/routes.js
+++ config/routes.js
@@ -2,4 +2,6 @@ module.exports = app => {
     app.get('/', app.app.controllers.womans.index)
 
     app.get('/cadastrar', app.app.controllers.womans.register)
+
+    app.post('/woman', app.app.controllers.womans.create)
 }
